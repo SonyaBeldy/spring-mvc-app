cd122598b62e8a7df3f70bc53f1ab46795fd9127
Sonya <sofya.beldy@mail.ru>
2023-03-21 18:39:39 +1000

    Get request with params by link

--- src/main/java/ru/sonyabeldy/springcourse/controllers/FirstController.java
+++ src/main/java/ru/sonyabeldy/springcourse/controllers/FirstController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping("/first")
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
