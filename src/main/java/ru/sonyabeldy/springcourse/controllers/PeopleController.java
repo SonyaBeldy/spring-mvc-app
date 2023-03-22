cba80edd1fc40105010ec86374b96aa3e923db4e
Sonya <sofya.beldy@mail.ru>
2023-03-22 23:23:11 +1000

    PersonDAO

---
+++ src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package ru.sonyabeldy.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sonyabeldy.springcourse.dao.PersonDAO;
+import ru.sonyabeldy.springcourse.models.Person;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        //Получим всех людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        //получим одного человека по id из DAO и передадим отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
