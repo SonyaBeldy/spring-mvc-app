435f3a051e09ccbf254c9e290c74db9472189329
Sonya <sofya.beldy@mail.ru>
2023-03-23 23:22:31 +1000

    ModelAttribure annotation, post

--- src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
@@ -3,9 +3,7 @@ package ru.sonyabeldy.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.sonyabeldy.springcourse.dao.PersonDAO;
 import ru.sonyabeldy.springcourse.models.Person;
 
@@ -33,4 +31,16 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
 }
