162a20c786e01f78891980c8ca5cfa85d16f0479
Sonya <sofya.beldy@mail.ru>
2023-03-25 23:19:46 +1000

    Delete

--- src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
@@ -55,6 +55,12 @@ public class PeopleController {
         return "redirect:/people";
     }
 
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
 
 
 }
