43bfd16a62f9ff23ea0e1bcaf17288f48d06949d
Sonya <sofya.beldy@mail.ru>
2023-03-30 16:47:40 +1000

    Validation

--- src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/sonyabeldy/springcourse/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package ru.sonyabeldy.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.sonyabeldy.springcourse.dao.PersonDAO;
 import ru.sonyabeldy.springcourse.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -38,7 +41,11 @@ public class PeopleController {
         return "people/new";
     }
     @PostMapping
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -50,7 +57,11 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
